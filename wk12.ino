144eded235f56b28bc96add3131d09105b6c9f04
charles841107 <33095468+charles841107@users.noreply.github.com>
2018-05-16 10:34:20 +0800

    Add files via upload

---
+++ wk12.ino
@@ -0,0 +1,19 @@
+const int buttonpin=2;
+const int ledpin=13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonpin, INPUT);
+  pinMode(ledpin, OUTPUT);
+  //digitalWrite(buttonpin,HIGH);
+}
+
+void loop() {
+  int buttonstate=digitalRead(buttonpin);
+  Serial.println(buttonstate);
+  delay(1);
+  if(buttonstate == HIGH){
+    digitalWrite(ledpin,HIGH);
+    }else{
+      digitalWrite(ledpin,LOW);
+      }
+}
